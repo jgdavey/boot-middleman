132133a8ac14424de1e35e7d9e92348f10c2edfc
Joshua Davey <josh@joshuadavey.com>
2015-02-11 19:34:10 -0600

    Always reset middleman application
    Class instance variables seem to last beyond instances of Ruby's
    ScriptingContainer instances.

--- resources/middleman_build.rb
+++ resources/middleman_build.rb
@@ -5,10 +5,15 @@ require "middleman-core/load_paths"
 Middleman.setup_load_paths
 
 require "middleman-core/cli"
+require "middleman-core/application"
+
+# Silly cache always needs reset
+Middleman::Cli::Build.instance_variable_set("@_shared_instance", nil)
 
 # Change directory to the root
 Dir.chdir(ENV["MM_ROOT"]) if ENV["MM_ROOT"]
 
-require "middleman-core/application"
 Middleman::Cli::Build.shared_instance.set(:build_dir, ENV["MM_BUILD"])
+
+# Do it!
 Middleman::Cli::Build.new([]).invoke(:build)
